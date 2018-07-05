53ac470dc2c1a7292ff7588e2548a54d66751fee
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2018-07-05 20:39:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+nanobox-db
+==========
+
+Connect to Nanobox.io Database
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/nanobox-db.svg)](https://npmjs.org/package/nanobox-db)
+[![CircleCI](https://circleci.com/gh/G3z/nanobox-db/tree/master.svg?style=shield)](https://circleci.com/gh/G3z/nanobox-db/tree/master)
+[![Appveyor CI](https://ci.appveyor.com/api/projects/status/github/G3z/nanobox-db?branch=master&svg=true)](https://ci.appveyor.com/project/G3z/nanobox-db/branch/master)
+[![Codecov](https://codecov.io/gh/G3z/nanobox-db/branch/master/graph/badge.svg)](https://codecov.io/gh/G3z/nanobox-db)
+[![Downloads/week](https://img.shields.io/npm/dw/nanobox-db.svg)](https://npmjs.org/package/nanobox-db)
+[![License](https://img.shields.io/npm/l/nanobox-db.svg)](https://github.com/G3z/nanobox-db/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.js
@@ -0,0 +1,95 @@
+const { Command, flags } = require('@oclif/command');
+const { exec } = require('child_process');
+const { read } = require('node-yaml');
+
+class NanoboxDbCommand extends Command {
+	async run() {
+		const { flags } = this.parse(NanoboxDbCommand);
+		const env = await this.getEnv(flags.env);
+		if (env) {
+			const dbType = await this.getDBType();
+			const cmd = `open ${dbType}://${env.user}:${env.pass}@${
+				env.host
+			}/gonano`;
+			this.log(`running ${cmd}`);
+			this.execute(cmd);
+		}
+	}
+
+	async getEnv(env = 'local') {
+		let nanobox_output = await this.execute(`nanobox info ${env}`);
+		return this.parseEnv(nanobox_output);
+	}
+
+	parseEnv(nanobox_output) {
+		if (nanobox_output.indexOf('Whoops,') !== -1) {
+			this.log(nanobox_output);
+			return;
+		}
+		const lines = nanobox_output.split('\n');
+
+		const user = this.searchInOutput(lines, 'DATA_DB_USER');
+		const host = this.searchInOutput(lines, 'DATA_DB_HOST');
+		const pass = this.searchInOutput(lines, 'DATA_DB_PASS');
+
+		return {
+			user,
+			host,
+			pass
+		};
+	}
+
+	searchInOutput(lines, variable) {
+		let line = lines.filter(l => l.indexOf(variable + ' ') !== -1).pop();
+		if (!line) {
+			return;
+		}
+		return line
+			.split(' = ')
+			.pop()
+			.trim();
+	}
+
+	async getDBType() {
+		const boxfile = await this.parseBoxFile();
+		const data_image = boxfile['data.db'].image;
+		if (data_image.indexOf('mysql') !== -1) {
+			return 'mysql';
+		} else if (data_image.indexOf('postgres') !== -1) {
+			return 'postgres';
+		}
+	}
+
+	async parseBoxFile() {
+		const path = process.cwd() + '/boxfile.yml';
+		return new Promise((success, fail) => {
+			read(path, (err, data) => {
+				if (err) {
+					fail(err);
+				} else {
+					success(data);
+				}
+			});
+		});
+	}
+
+	execute(command) {
+		return new Promise(callback => {
+			exec(command, function(error, stdout, stderr) {
+				callback(stdout);
+			});
+		});
+	}
+}
+
+NanoboxDbCommand.description = `Open the database of the current nanobox project in your favorite IDE`;
+
+NanoboxDbCommand.flags = {
+	// add --version flag to show CLI version
+	version: flags.version({ char: 'v' }),
+	// add --help flag to show CLI version
+	help: flags.help({ char: 'h' }),
+	env: flags.string({ char: 'e', description: 'name to print' })
+};
+
+module.exports = NanoboxDbCommand;
